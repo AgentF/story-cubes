8cf7384de944357d82407a02405573552a1d11f2
Freddy Mota <freddyfx231@gmail.com>
2019-08-07 09:23:36 -0400

    Finished basic dices functionality

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # story-cubes
 
-This is a story cubes app
+##Clarification
+This is a StoryCubes app made as a personal project, this isn't an official “Rory’s Story Cubes” app and I'm not affiliated in any way with Rory's Story Cubes®.
+
+“Rory’s Story Cubes” is the trade mark of the Asmodee Group and all intellectual property rights in the name, trade mark and game “Rory’s Story Cubes” is the property of Asmodee Group.
+
+###About the app
+
+I was inspired to do an StoryCubes app when I heard of the game's idea and how that can help you stimulate your creativity and storytelling.
+
+Obviusly this is a very open and creative game and with just the dices works but my aim is implementing various game modes, a quick story mode where you write a short story (140-280char) in a short period of time (3-5min), a collaborative mode where each participant make only a part of the history and the next begins from there.
+
+I choose to keep it just html, css and vanilla js as long as it works, maybe in the future I'll redo it with React.js
+
+#### To Do:
+  - [x] Make a small dices collection.
+  - [ ] Add to the dices collection.
+  - [ ] Organize the dices collection by common themes.
+  - [x] Select the dices to play.
+  - [ ] Make some sort of dice animation.
+  - [x] Implement random selection.
+  - [ ] Implement a smart textarea to write the stories.
+  - [ ] Implement an export story to twitter feature to tweet your story in various separeted twits.
+  - [ ] Make an interface an organize the app screens.
--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
+/* This should be in another file */
 const dicesStored = [
   {
-    id: 0,
+    id: 'dice-0',
     show: true,
     name: 'action',
     sketches: [
@@ -25,7 +26,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 1,
+    id: 'dice-1',
     show: true,
     name: 'characters',
     sketches: [
@@ -50,7 +51,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 2,
+    id: 'dice-2',
     show: true,
     name: 'powers',
     sketches: [
@@ -75,7 +76,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 3,
+    id: 'dice-3',
     show: true,
     name: 'places',
     sketches: [
@@ -100,7 +101,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 4,
+    id: 'dice-4',
     show: true,
     name: 'daily',
     sketches: [
@@ -125,7 +126,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 5,
+    id: 'dice-5',
     show: true,
     name: 'detective',
     sketches: [
@@ -150,7 +151,7 @@ const dicesStored = [
     ]
   },
   {
-    id: 6,
+    id: 'dice-6',
     show: false,
     name: 'characters',
     sketches: [
@@ -175,18 +176,66 @@ const dicesStored = [
     ]
   },
 ];
+/* This should be in another file */
 
+/* Getting DOM elements */
 const dicesContainer = document.querySelector('.dices-container');
+const playButton = document.querySelector('#play');
+const shuffleButton = document.querySelector('#shuffle');
+const replayButton = document.querySelector('#replay');
+/* Getting DOM elements */
+
+/* Setting variables */
 let dicesID = [];
 const requiredDices = 3;
-let eventsSeted = false;
+let status = 0;
+/* Setting variables */
+
+/* Declaring functions */
+const getDiceResult = () => Math.floor(Math.random() * Math.floor(6));
+const getRandomDice = () => Math.floor(Math.random() * Math.floor(dicesStored.filter(({ show }) => show ).length));
 
 const refresh = () => {
-  showDices(dicesContainer);
+  if (status === 0 || dicesID.length < requiredDices) {
+    status = 1;
+  }
+
+  if (dicesID.length === requiredDices && status === 1) {
+    status = 2;
+  }
+
+  if (status === 2) {
+    playButton.classList.remove('disabled-button');
+    playButton.removeAttribute('disabled');
+  } else {
+    playButton.classList.add('disabled-button');
+    playButton.setAttribute('disabled', '');
+  }
+
+  if (status < 3) {
+    shuffleButton.classList.remove('disabled-button');
+    shuffleButton.removeAttribute('disabled');
+  } else {
+    shuffleButton.classList.add('disabled-button');
+    shuffleButton.setAttribute('disabled', '');
+  }
+
+  if (status === 3) {    
+    playButton.classList.add('disabled-button');
+    playButton.setAttribute('disabled', '');
+  }
+
+  //console.log({ status, dicesID: JSON.parse(JSON.stringify(dicesID)) });
+
+  if (status > 2) {
+    usableDices(dicesContainer);
+  } else {
+    selectionDices(dicesContainer);
+  }
 }
 
 const toggleDice = (id) => {
-  const check = dicesID.findIndex(diceID => `check-${diceID}` === `check-${id}`)
+  const check = dicesID.findIndex(diceID => diceID === id)
   if (check === -1) {
     dicesID.push(id);
     if (dicesID.length > requiredDices) {
@@ -195,33 +244,77 @@ const toggleDice = (id) => {
   } else {
     dicesID = [...dicesID.slice(0, check), ...dicesID.slice(check + 1)]
   }
-  refresh()
+  refresh();
 }
 
-const getDiceResult = () => Math.floor(Math.random() * Math.floor(6));
+const goPlay = () => {
+  if (status !== 2) return;
+  status = 3;
+  refresh();
+};
 
-const showDices = (DOMelement) => {
-  DOMelement.innerHTML = dicesStored.reduce((dicesStoredAcc, { show, sketches, name, id }) => show ? dicesStoredAcc + `
-    <div class="dice">
-      <div class="dice-name">${name}</div>
-      <div class="faces-container">${sketches.reduce((facesAcc, { sketch, style }, i) => facesAcc + `
-        <div class="face" style="grid-area: face-${i}">
+const goShuffle = () => {
+  if (status > 2) return;
+  let randomDice;
+  do {
+    randomDice = `dice-${getRandomDice()}`;
+  } while(dicesID.findIndex(diceID => diceID === randomDice) !== -1);
+  toggleDice(randomDice);
+};
+
+const goReplay = () => {
+  status = 1;
+  dicesID.length = 0;
+  refresh();
+};
+
+const selectionDices = (DOMelement) => {
+  const dicesString = dicesStored.reduce((dicesStoredAcc, { show, sketches, name, id }) => show ? dicesStoredAcc + `
+      <div class="dice">
+        <div class="faces-container">${sketches.reduce((facesAcc, { sketch, style }, i) => facesAcc + `
+          <div class="face" style="grid-area: face-${i}">
+            <i class="material-icons" style="${style}">
+              ${sketch}
+            </i>      
+          </div>
+          `, '')}  
+        </div>
+        <div class="dice-footer">
+          <button class="transparent-button" title="check-button" data-diceid="${id}">
+            <i class="material-icons">
+              radio_button_${dicesID.find(diceID => diceID === id) ? 'checked' : 'unchecked'}
+            </i>
+          </button>
+        </div>
+      </div>
+    ` : dicesStoredAcc, '');
+
+  DOMelement.innerHTML = dicesString;
+  document.querySelectorAll('button[title="check-button"]').forEach(button => button.addEventListener('click', () => toggleDice(button.getAttribute('data-diceid'))));
+}
+
+const usableDices = (DOMelement) => {
+  const dicesString = dicesID.reduce((dicesAcc, diceID) => {
+    const dice = dicesStored.find(({ id }) => id === diceID);
+    const { style, sketch } = dice.sketches[getDiceResult()];
+    return (dicesAcc + `
+      <div class="dice">
+        <div class="face">
           <i class="material-icons" style="${style}">
             ${sketch}
           </i>      
-        </div>
-        `, '')}  
+        </div> 
       </div>
-      <div class="dice-footer">
-        <button class="check-button" id="check-${id}">
-          <i class="material-icons">
-            radio_button_${dicesID.find(diceID => diceID === `check-${id}`) ? 'checked' : 'unchecked'}
-          </i>
-        </button>
-      </div>
-    </div>
-  ` : dicesStoredAcc, '');
-  document.querySelectorAll('.check-button').forEach(button => button.addEventListener('click', () => toggleDice(button.id)));
+    `);
+  }, '');
+  DOMelement.innerHTML = dicesString;
 }
+/* Declaring functions */
+
+/* Setting events */
+playButton.addEventListener('click', goPlay);
+shuffleButton.addEventListener('click', goShuffle);
+replayButton.addEventListener('click', goReplay);
+/* Setting events */
 
 refresh();
